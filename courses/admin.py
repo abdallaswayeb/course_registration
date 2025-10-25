57260b909fcde7126148b1a443b10b220757be1e
abdallaswayeb <141945714+abdallaswayeb@users.noreply.github.com>
2025-10-25 22:26:21 +0200

    🛠️ Register Student in admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ('student_id', 'first_name', 'last_name', 'email', 'is_active', 'created_at')
+	search_fields = ('first_name', 'last_name', 'email', 'student_id')
+	list_filter = ('is_active',)
