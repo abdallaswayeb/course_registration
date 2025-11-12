76e5155aed888900012f25227bc79a04598d6f15
abdallaswayeb <141945714+abdallaswayeb@users.noreply.github.com>
2025-11-12 20:41:39 +0200

    update urls

--- course_registration/urls.py
+++ course_registration/urls.py
@@ -32,4 +32,16 @@ urlpatterns = [
     path('students/add/', views.add_student, name='add_student'),
     path('sections/add/', views.add_section, name='add_section'),
     path('materials/add/', views.add_material_page, name='add_material_page'),
+    path('sections/<int:id>/edit/', views.edit_section, name='edit_section'),
+    path('materials/<int:material_id>/', views.material_detail, name='material_detail'),
+    path('grades_entry/', views.grades_entry, name='grades_entry'),
+    path('add_grade_entry/', views.add_grade_entry, name='add_grade_entry'),
+    path('procedures/', views.procedures_page, name='procedures_page'),
+    path('materials_download/', views.materials_download_page, name='materials_download_page'),
+    path('students/materials_download/', views.student_material_download, name='student_material_download'),
+    path('students/edit_student_downloads/', views.edit_student_downloads, name='edit_student_downloads'),
+    path('procedures/material_prerequisites/', views.manage_material_prerequisites, name='manage_material_prerequisites'),
+    path('procedures/timetable/', views.timetable_page, name='timetable_page'),
+    path('procedures/timetable/save/', views.save_lecture, name='save_lecture'),
+
 ]
